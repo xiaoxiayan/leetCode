4e62f651828c2974f120672f231bd5ceb08d7032
xiaoxiayan <a690150618@qq.com>
2021-12-15 20:14:51 +0800

    aaa

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// 1 、两数之和 
+// 1 、两数之和
 // 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
 // 你可以按任意顺序返回答案。
 /**
- * 
+ *
 输入：nums = [2,7,11,15], target = 9
 输出：[0,1]
 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
 
- * 
- * 
- * @param {*} nums 
- * @param {*} target 
- * @returns 
- * 
- * 
- * 
+ *
+ *
+ * @param {*} nums
+ * @param {*} target
+ * @returns
+ *
+ *
+ *
  */
  
 
@@ -41,7 +41,7 @@
 //     }
 // };
 /**
- * 
+ *
 
  * 2、两数相加
 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
@@ -69,14 +69,14 @@
 //          this.val = (val===undefined ? 0 : val)
 //          this.next = (next===undefined ? null : next)
 //      }
-     
+
 // // fn1
 //  var addTwoNumbers = function(l1, l2) {
 //     let head = null, tail = null;
 //     let carry = 0; // 进位符
 //    while (l1 || l2) {
 //        // 判断 这一位数 是否有，有就 是val, 没有就是 0
-//        const n1 = l1 ? l1.val : 0; 
+//        const n1 = l1 ? l1.val : 0;
 //        const n2 = l2 ? l2.val : 0;
 //        // 这一位数，相当于 n1+ n2 + 进位数。
 //        const sum = n1 + n2 + carry;
@@ -86,9 +86,9 @@
 //        } else {
 //            // 如果已经有和 head 首位 -->  尾部 next --> 链表的下一节 == new ListNode
 //            tail.next = new ListNode(sum % 10);
-//            // 尾部 重新指向 fn 里面的 next 
+//            // 尾部 重新指向 fn 里面的 next
 //            tail = tail.next;
-           
+
 //        }
 //        // 向下取整
 //        carry = Math.floor(sum / 10);
@@ -113,8 +113,8 @@
 // node1.next = node2;
 // node2.next = node3
 
-    
-    
+
+
 // let l1 = new ListNode(4)
 // let l2 = new ListNode(5)
 // let l3 = new ListNode(6)
@@ -150,9 +150,9 @@
 /**
  * 无重复字符的最长子串
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
- * 
+ *
  * 输入: s = "abcabcbb"
-   输出: 3 
+   输出: 3
    解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 示例 2:
 
@@ -171,12 +171,12 @@
 
 输入: s = ""
 输出: 0
- * 
- * 
+ *
+ *
  */
 // 思考： 把字符传拆开，然后用数组 进去push, 取最长
  /**
- * @param {string} 
+ * @param {string}
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
@@ -193,11 +193,11 @@ var lengthOfLongestSubstring = function(s) {
             }
             newArr.push(s[i])
             maxLength = Math.max(maxLength, newArr.length)
-             
+
         }
     }
     return maxLength
-}; 
+};
 console.log(lengthOfLongestSubstring('avdlvhf'))
 // var lengthOfLongestSubstring = function(s) {
 //     // 哈希集合，记录每个字符是否出现过
@@ -257,8 +257,8 @@ console.log(lengthOfLongestSubstring('avdlvhf'))
     let arr = nums1.concat(nums2).sort(function(a, b){return a - b})
     // 中位数分为 偶数 和 奇数的中位。偶数就是取 值的 max && min ，奇数就是取中间
     let midIndex = arr.length/2
-    return !!(arr.length%2) ? (arr[Math.floor(midIndex)]) : ((arr[midIndex] + arr[midIndex -1] )/2) 
-   
+    return !!(arr.length%2) ? (arr[Math.floor(midIndex)]) : ((arr[midIndex] + arr[midIndex -1] )/2)
+
 };
 // 6. Z 字形变换
 //
@@ -308,3 +308,6 @@ var convert = function(s, numRows) {
   }
   return res
   };
+
+
+  // leia
