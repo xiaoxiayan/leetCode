72a1a42ea05c18cd0a9bc44a6f908763b2f78951
xiaoxiayan <a690150618@qq.com>
2021-12-22 12:47:50 +0800

    6

--- 回溯题目/index.js
+++ 回溯题目/index.js
@@ -48,31 +48,89 @@
 
 // 你可以按 任何顺序 返回答案。
 
-var combine = function(n, k) {
-  let result = [];
-  /**
-   *
-   * @param {*} start 开始的数字
-   * @param {*} path 已选择的列表，或者说已经走过的路
-   */
-  let dfs = (start, path) => {
-    // 如果已经选满了的话，加入结果集中
-    if (path.length == k) {
-      result.push(path.slice());
-      return;
-    }
-    // 从开始的数字到末尾的数字
-    for (let i = start; i <= n; i++) {
-      // 加入选择列表中
-      path.push(i);
-      // 继续深度搜索
-      dfs(i + 1, path);
-      // 撤销选择
-      path.pop(i);
-    }
-  };
-  dfs(1, []);
-  return result;
+// var combine = function(n, k) {
+//   let result = [];
+//   /**
+//    *
+//    * @param {*} start 开始的数字
+//    * @param {*} path 已选择的列表，或者说已经走过的路
+//    */
+//   let dfs = (start, path) => {
+//     // 如果已经选满了的话，加入结果集中
+//     if (path.length == k) {
+//       result.push(path.slice());
+//       return;
+//     }
+//     // 从开始的数字到末尾的数字
+//     for (let i = start; i <= n; i++) {
+//       // 加入选择列表中
+//       path.push(i);
+//       // 继续深度搜索
+//       dfs(i + 1, path);
+//       // 撤销选择
+//       path.pop(i);
+//     }
+//   };
+//   dfs(1, []);
+//   return result;
+// };
+
+/** 216. 组合总和 III */
+/** 找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
+
+说明：
+
+所有数字都是正整数。
+解集不能包含重复的组合。 */
+/**
+ * 示例 1:
+
+输入: k = 3, n = 7
+输出: [[1,2,4]]
+示例 2:
+
+输入: k = 3, n = 9
+输出: [[1,2,6], [1,3,5], [2,3,4]]
+ *
+*/
+  // 或Sn=
+// var maxV = k => k * (9 + 10 - k) / 2;
+// var minV = k => k * (1 + k) / 2;
+// 等差数列前 N项和 公式 Sn = [n*(a1+an)]/2 。  因为当前数列是 1-9， 相当于等差 1 的数列 d = 1
+var maxV = k => k * (9 + 10 - k) / 2;  // 求前N项和最大值 MaxV  a1 = 9
+var minV = k => k * (1 + k) / 2;  // 求前N项和最小值 a1 = 1
+var combinationSum3 = function(k, n) {
+  if (k > 9 || k < 1) return [];
+  // if (n > maxV(k) || n < minV(k)) return [];
+  // if (n === maxV(k)) return [Array.from({length: k}).map((v, i) => 9 - i)];
+  // if (n === minV(k)) return [Array.from({length: k}).map((v, i) => i + 1)];
+
+  const res = [], path = [];
+  backtracking(k, n, 1, 0);
+  return res;
+  function backtracking(k, n, i, sum){
+      const len = path.length;
+      if (len > k || sum > n) return;
+      console.log('maxV==',maxV(k - len), n - sum )
+      console.log('minV==',minV(k - len), n - sum)
+      if (maxV(k - len) < n - sum) return;
+      if (minV(k - len) > n - sum) return;
+
+      if(len === k && sum == n) {
+          res.push(Array.from(path));
+          return;
+      }
+      // 定义最小值 等于   和 -  结果， 9 + 长度 - k + 1
+      const min = Math.min(n - sum, 9 + len - k + 1);
+      debugger
+      for(let a = i; a <= min; a++) {
+          path.push(a);
+          sum += a;
+          backtracking(k, n, a + 1, sum);
+          path.pop();
+          sum -= a;
+      }
+  }
 };
 
-console.log(combine(4, 2))
+console.log(combinationSum3(3, 12))
