f73137aebcb53b8369d5783a5aadd776c848e272
xiaoxiayan <a690150618@qq.com>
2022-01-01 14:55:10 +0800

    47

--- 回溯题目/sort.js
+++ 回溯题目/sort.js
@@ -29,3 +29,68 @@
   backtracking(0)
   return res
 };
+
+/**
+ * 47. 全排列 II
+给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+
+*/
+var permute = function(nums) {
+    let path = [], res = []
+    const len = nums.length
+    // 思路。循环数组，递归深入
+    function backtracking (i) {
+        // 终止条件
+        if(path.length === len) {
+            res.push(Array.from(path))
+            return
+        }
+        for(let j = 0; j < len; j++) {
+            // 标记一下，循环中 下位如果和前位 相同就 跳过
+            // if(path.includes(nums[j] )) continue;
+
+            path.push(nums[j])
+            backtracking()
+            path.pop()
+        }
+    }
+  
+    backtracking(0)
+    return res
+  };
+
+  var permuteUnique = function (nums) {
+    nums.sort((a, b) => {
+        return a - b
+    })
+    let result = []
+    let path = []
+
+    function backtracing( used) {
+        if (path.length === nums.length) {
+            result.push(path.slice())
+            return
+        }
+        for (let i = 0; i < nums.length; i++) {
+            // !used  标记一下 集合 中这个元素是否使用过
+            if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) {
+                continue
+            }
+            // 第一次进来，!used[i] 是不存在的 可以去使用
+            if (!used[i]) {
+                // 标记一下 这一层中，这个元素已经使用了。
+                used[i] = true
+                path.push(nums[i])
+                backtracing(used)
+                path.pop()
+                used[i] = false
+            }
+
+
+        }
+    }
+    backtracing([])
+    return result
+};
+
+permuteUnique([1,1,2])
