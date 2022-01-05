c1921df4cb03cb8f92533a5e937eacee344470be
xiaoxiayan <a690150618@qq.com>
2022-01-05 10:09:12 +0800

    排序找路线

--- 回溯题目/other.js
+++ 回溯题目/other.js
@@ -34,4 +34,53 @@ var findSubsequences = function(nums) {
   return res
 };
 
-findSubsequences([4,6,7,7])
+// findSubsequences([4,6,7,7])
+
+/**
+ * 332. 重新安排行程
+ *给你一份航线列表 tickets ，其中 tickets[i] = [fromi, toi] 表示飞机出发和降落的机场地点。请你对该行程进行重新规划排序。
+
+所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。如果存在多种有效的行程，请你按字典排序返回最小的行程组合。
+
+例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"] 相比就更小，排序更靠前。
+假定所有机票至少存在一种合理的行程。且所有的机票 必须都用一次 且 只能用一次。
+ *
+ *  */
+
+
+var findItinerary = function(tickets) {
+  const graph = getGraph(tickets)
+  const ans = []
+  dfs(graph,'JFK', ans)
+  //深度优先遍历是先进先出，所以reverse反转一下
+  return ans.reverse()
+};
+//从JFS出发，深度优先遍历
+function dfs(graph, cur, ans){
+  // 题目要求就是 在目的地中 搜索出最佳的路线条件（按照最小的字母排序），前提是 从JFK 出发。
+  if(!graph.has(cur)) return
+  const neighbors = graph.get(cur)
+  //题目要求先遍历字典序小的元素
+  while(neighbors.length) dfs(graph, neighbors.shift(), ans)
+  // 因为深度优先遍历是先进先出，所以每次遍历出发点添加到最后，意为最先出
+  ans.push(cur)
+}
+// 先存储所有机场之间的关系，哪怕tickets中没有给某个机场提供目的地记录，也要把它的目的地置为空数组
+function getGraph(tickets){
+  const map = new Map()
+  // 收集每一个出发点的所有目的地
+  for(let i = 0;i < tickets.length;i++){
+      const from = tickets[i][0]
+      const to = tickets[i][1]
+      if(!map.has(from)) map.set(from, [])
+      if(!map.has(to)) map.set(to, [])
+      map.get(from).push(to)
+  }
+  for(let [key, value] of map){
+      //字典顺序排序目的地数组
+      value.sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
+  }
+  return map
+}
+
+findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]])
