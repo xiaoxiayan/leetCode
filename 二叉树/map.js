a8ac09b76471ea8be4eeb511390d2dc24c2b1497
xiaoxiayan <a690150618@qq.com>
2022-01-07 16:16:18 +0800

    二叉树

---
+++ 二叉树/map.js
@@ -0,0 +1,47 @@
+ /**  Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+// 生成节点的 函数
+  function TreeNode(val, left, right) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+
+// 144 .二叉树的前序遍历
+// 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+// 输入：root = [1,null,2,3]
+// 输出：[1,2,3]
+前序遍历:
+
+var preorderTraversal = function(root, res = []) {
+    if (!root) return res;
+    res.push(root.val);
+    preorderTraversal(root.left, res)
+    preorderTraversal(root.right, res)
+    return res;
+};
+
+中序遍历:
+
+var inorderTraversal = function(root, res = []) {
+    if (!root) return res;
+    inorderTraversal(root.left, res);
+    res.push(root.val);
+    inorderTraversal(root.right, res);
+    return res;
+};
+
+后序遍历:
+
+var postorderTraversal = function(root, res = []) {
+    if (!root) return res;
+    postorderTraversal(root.left, res);
+    postorderTraversal(root.right, res);
+    res.push(root.val);
+    return res;
+};
