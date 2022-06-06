b0f63393256588867e269ad83e9abc0bdaf01cb3
xiaoxiayan <a690150618@qq.com>
2022-06-06 20:27:02 +0800

    dd

--- index.js
+++ index.js
@@ -595,5 +595,62 @@ function getNextStatus(geneStr, m) {
   return nextStatus;
 }
 
-const res =  minMutation("AACCTTGG", "AATTCCGG", ["AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"])
-console.log(res)
+// const res =  minMutation("AACCTTGG", "AATTCCGG", ["AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"])
+// console.log(res)
+
+// 732. 我的日程安排表 III
+// 当 k 个日程安排有一些时间上的交叉时（例如 k 个日程安排都在同一时间内），就会产生 k 次预订。
+// 给你一些日程安排 [start, end) ，请你在每个日程安排添加后，返回一个整数 k ，表示所有先前日程安排会产生的最大 k 次预订。
+// 实现一个 MyCalendarThree 类来存放你的日程安排，你可以一直添加新的日程安排。
+// 输入：
+// ["MyCalendarThree", "book", "book", "book", "book", "book", "book"]
+// [[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
+// 输出：
+// [null, 1, 1, 2, 3, 3, 3]
+
+// 解释：
+// MyCalendarThree myCalendarThree = new MyCalendarThree();
+// myCalendarThree.book(10, 20); // 返回 1 ，第一个日程安排可以预订并且不存在相交，所以最大 k 次预订是 1 次预订。
+// myCalendarThree.book(50, 60); // 返回 1 ，第二个日程安排可以预订并且不存在相交，所以最大 k 次预订是 1 次预订。
+// myCalendarThree.book(10, 40); // 返回 2 ，第三个日程安排 [10, 40) 与第一个日程安排相交，所以最大 k 次预订是 2 次预订。
+// myCalendarThree.book(5, 15); // 返回 3 ，剩下的日程安排的最大 k 次预订是 3 次预订。
+// myCalendarThree.book(5, 10); // 返回 3
+// myCalendarThree.book(25, 55); // 返回 3
+
+var MyCalendarThree = function() {
+  this.times = {}
+};
+
+/**
+* @param {number} start
+* @param {number} end
+* @return {number}
+*/
+MyCalendarThree.prototype.book = function(start, end) {
+  this.times[start] = (this.times[start] || 0) + 1
+  this.times[end] = (this.times[end] || 0) - 1
+  let max = 0, total = 0
+  debugger
+  for (const key in this.times) {
+      total += this.times[key]
+      if (total > max) {
+          max = total
+      }
+  }
+  return max;
+};
+
+/**
+* Your MyCalendarThree object will be instantiated and called as such:
+*
+* var param_1 = obj.book(start,end)
+*/
+var myCalendarThree = new MyCalendarThree()
+
+ myCalendarThree.book(10, 20);
+ myCalendarThree.book(50, 60);
+ myCalendarThree.book(10, 40);
+console.log(myCalendarThree)
+
+
+
